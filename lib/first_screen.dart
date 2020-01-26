18bde0cd24a1b0e5f09da04b314f4fd0150ec9a5
jahidRatul <jahidratul@gmail.com>
2020-01-26 14:50:33 +0600

    signout added

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
 
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(color: Colors.blue[100]),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 40),
+            RaisedButton(
+              onPressed: () {
+                signOutGoogle();
+                Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (context) {
+                  return LoginPage();
+                }), ModalRoute.withName('/'));
+              },
+              color: Colors.deepPurple,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Sign Out',
+                  style: TextStyle(fontSize: 25, color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
